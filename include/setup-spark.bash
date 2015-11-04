42cc4e344d0e68015fe5315c3a36ea0c8b83fec6
Omar Padron <omar.padron@kitware.com>
2015-11-04 12:21:22 -0500

    add hadoop variables to setup-spark.bash include script

--- include/setup-spark.bash
+++ include/setup-spark.bash
@@ -1,5 +1,20 @@
 
-source "$( ls -1 /opt/spark/*/conf/spark-env.sh | sort | tail -n 1 )"
+get_top_file() {
+    ls -1 $1 | sort | tail -n 1
+}
+
+hadoop_conf_file="$( get_top_file "/opt/hadoop/*/libexec/hadoop-config.sh" )"
+spark_conf_file="$( get_top_file "/opt/spark/*/conf/spark-env.sh" )"
+
+source "$hadoop_conf_file"
+source "$spark_conf_file"
+
+if [ -z "$HADOOP_VERSION" ] ; then
+    export HADOOP_VERSION="$(
+        basename "$(
+            dirname "$(
+                dirname "$hadoop_conf_file" )" )" )"
+fi
 
 export PYTHONPATH="$SPARK_HOME/python/lib/py4j-0.8.2.1-src.zip:$PYTHONPATH"
 export PYTHONPATH="$SPARK_HOME/python:$PYTHONPATH"
